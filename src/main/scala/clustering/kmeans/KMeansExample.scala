77af90c8d774d3100991095dbca301a29602d540
Najib <najib.mahjoubi@gmail.com>
2017-07-01 14:52:07 +0200

    elbow method + trainvalidation

--- src/main/scala/clustering/kmeans/KMeansExample.scala
+++ src/main/scala/clustering/kmeans/KMeansExample.scala
@@ -1,29 +1,15 @@
 
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.mllib.clustering.{KMeans => MLlibKMeans}
-import org.apache.spark.sql.functions.{udf, col}
-import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.clustering.KMeans.{K_MEANS_PARALLEL, RANDOM}
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.spark.sql.SaveMode
+
 
 
 object KMeansExample {
-
-
-  def defineKMeans(nbCluster: Int): KMeans = {
-    new KMeans()
-        .setK(nbCluster)
-        .setFeaturesCol("features")
-        .setPredictionCol("prediction")
-        .setMaxIter(20)
-        .setTol(0.0001)
-        .setInitMode(MLlibKMeans.K_MEANS_PARALLEL)
-        .setInitSteps(20)
-        .setSeed(1L)
-  }
-
-
-  val append = udf((x: Double, y: Double) => Vectors.dense(x, y))
-
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
@@ -34,15 +20,43 @@ object KMeansExample {
 
     val path = "data/clustering"
     var data = spark.read.parquet(path)
-    data = data.select(append(col("x"), col("y")).alias("features"))
-    data.show(5)
+                .rdd
+                .map(p => Vectors.dense(p.getDouble(p.fieldIndex("x")), p.getDouble(p.fieldIndex("y")))) 
+                .persist()
 
-    // Trains a k-means model.
-    
-    val prediction = defineKMeans(16).fit(data).transform(data)
-    prediction.write.parquet("target/model/kmeans")
+   // Elbow method
+   val elbowCost = computeElbowMethod(spark, data, data)
+   elbowCost.write.mode(SaveMode.Overwrite).parquet("target/data/elbowcost")
+
+   // Train validation
+   val trainRatio = 0.75
+   val Array(training, validation) = data.randomSplit(Array(trainRatio, 1.0 - trainRatio), 123L)
+   training.persist()
+   validation.persist()
+   //training.write.mode(SaveMode.Overwrite).parquet("target/data/trainvalidation/training")
+   //validation.write.mode(SaveMode.Overwrite).parquet("target/data/trainvalidation/validation")
+   val trainValidationCost = computeElbowMethod(spark, training, validation)
+   trainValidationCost.write.mode(SaveMode.Overwrite).parquet("target/data/trainvalidation/elbowcost")
 
     spark.stop()
   }
+
+  def computeKMeansModel(data: RDD[Vector], nbCluster: Int): KMeansModel = {
+     new KMeans()
+      .setK(nbCluster)
+      .setMaxIterations(20)
+      .setInitializationSteps(10)
+      .setInitializationMode(K_MEANS_PARALLEL)
+      .run(data)}
+
+  def computeElbowMethod(spark: SparkSession, training: RDD[Vector], validation: RDD[Vector]): DataFrame = {
+    var resultElbow: List[(Int, Double)] = List()
+    (4 to 20 by 1).foreach(nbCluster => {
+         val cost: Double = computeKMeansModel(training, nbCluster).computeCost(validation)
+         resultElbow = resultElbow ++ List((nbCluster, cost))})
+    resultElbow
+    spark.createDataFrame(resultElbow).toDF("k", "cost")
+}
+
 }
 // scalastyle:on println
