a186a32de8b3464f180d3eb8e5f4ac1c3c900c31
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-16 17:47:32 +0200

    add clustercenters method

--- src/main/scala/fr/spark/evaluation/clustering/KMeansTask.scala
+++ src/main/scala/fr/spark/evaluation/clustering/KMeansTask.scala
@@ -4,6 +4,7 @@ import org.apache.spark.ml.clustering.KMeansModel
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.mllib.clustering.{KMeans => MLlibKMeans}
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.linalg.Vector
 
 class KMeansTask(val featuresColumn: String, predictionColumn: String) extends ClusteringFactory {
 
@@ -27,4 +28,8 @@ class KMeansTask(val featuresColumn: String, predictionColumn: String) extends C
   override def transform(data: DataFrame): DataFrame = model.transform(data)
 
   override def computeCost(data: DataFrame): Double = model.computeCost(data)
+
+  override def clusterCenters(): Array[Vector] = {
+    model.clusterCenters
+  }
 }
